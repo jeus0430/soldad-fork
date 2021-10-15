0137d994f73dfc4d3e5b752bfd836a6bde1b246f
jeus0430 <jeus0430.tazan@gmail.com>
2021-10-15 09:28:05 +0300

    club responsive complete

---
+++ src/components/club/index.js
@@ -0,0 +1,47 @@
+import "./style.scss"
+import Discord from "../../resources/images/discord-black.svg"
+import Twitter from "../../resources/images/twitter-black.svg"
+import Bullish from "../../resources/images/bullish.svg"
+
+const Club = () => {
+  return (
+    <div className="club">
+      <div className="container-club">
+        <div className="community-container">
+          <div className="community-piece">
+            <span className="community-piece-text">Community Member</span>
+            <span className="community-piece-info">
+              <img src={Discord} />
+              9,000 +
+            </span>
+          </div>
+          <div className="community-piece">
+            <span className="community-piece-text">Followers on twitter</span>
+            <span className="community-piece-info">
+              <img src={Twitter} />
+              6,000 +
+            </span>
+          </div>
+          <div className="community-piece">
+            <span className="community-piece-text">Llama owners</span>
+            <span className="community-piece-info">
+              <img src={Bullish} />
+              1,100 +
+            </span>
+          </div>
+        </div>
+        <h1>Bullish Club is a community of humans who believe in a crypto future.</h1>
+        <div className="newspaper">
+          <div>
+            Passion for NFTs connected our international team from the US and Europe with the remarkable creator FlumenFox4. Bullish club is starting with a collection of 10,000 Bullish Llamas NFTs—unique digital collectibles living on the Ethereum blockchain.
+          </div>
+          <div>
+            As a Bullish Llama owner, you gain access to a Bullish Club app which will allow you to collect and herd your Llamas in one place. Future areas and perks can be unlocked by the community through roadmap activation together with charity donations.
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Club
--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom"
 import Layout from "../../components/layout"
 import Introduction from "components/introduction"
+import Club from "components/club"
 import "./style.scss"
 
 const Home = () => {
   return (
     <Layout>
       <Introduction />
+      <Club />
     </Layout>
   )
 }
