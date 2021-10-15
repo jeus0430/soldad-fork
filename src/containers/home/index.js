4699caef03f626c6ee59e9bbca4d294cdc3d5f35
jeus0430 <jeus0430.tazan@gmail.com>
2021-10-15 04:11:26 +0300

    remove all

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,18 +1,10 @@
 import { Link } from "react-router-dom"
 import Layout from "../../components/layout"
 import "./style.scss"
-import Dad from "../../components/dad"
-import About from "../../components/about"
-import Benefit from "../../components/benefit"
-import DadLeft from "../../resources/images/dad-left.jpg"
-import DadRight from "../../resources/images/dad-right.jpg"
 
 const Home = () => {
   return (
     <Layout>
-      <Dad />
-      <About />
-      <Benefit />
     </Layout>
   )
 }
